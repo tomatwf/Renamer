d31cd2c099f1c88865e92c396f9a91e461996523
DESKTOP-Q42TH3L\toom <kccheng@cuhk.edu.hk>
2022-03-27 18:56:56 +0800

    Add project files.

---
+++ Renamer/Form1.Designer.cs
@@ -0,0 +1,231 @@
+﻿
+namespace Renamer
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripbtnOpenFolder = new System.Windows.Forms.ToolStripButton();
+            this.toolStripbtnOpenAuth = new System.Windows.Forms.ToolStripButton();
+            this.toolStripProgressBar = new System.Windows.Forms.ToolStripProgressBar();
+            this.toolStripLbPathInfo = new System.Windows.Forms.ToolStripTextBox();
+            this.dgRename = new System.Windows.Forms.DataGridView();
+            this.before = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.after = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnRunAPI = new System.Windows.Forms.Button();
+            this.btnRefreash = new System.Windows.Forms.Button();
+            this.btnRenameAll = new System.Windows.Forms.Button();
+            this.lbStatus = new System.Windows.Forms.Label();
+            this.lbImage = new System.Windows.Forms.PictureBox();
+            this.tbConsole = new System.Windows.Forms.TextBox();
+            this.toolStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgRename)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lbImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripbtnOpenFolder,
+            this.toolStripbtnOpenAuth,
+            this.toolStripProgressBar,
+            this.toolStripLbPathInfo});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(1074, 28);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripbtnOpenFolder
+            // 
+            this.toolStripbtnOpenFolder.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripbtnOpenFolder.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripbtnOpenFolder.Name = "toolStripbtnOpenFolder";
+            this.toolStripbtnOpenFolder.Size = new System.Drawing.Size(74, 25);
+            this.toolStripbtnOpenFolder.Text = "Open folder";
+            this.toolStripbtnOpenFolder.Click += new System.EventHandler(this.toolStripbtnOpenFolder_Click);
+            // 
+            // toolStripbtnOpenAuth
+            // 
+            this.toolStripbtnOpenAuth.AutoToolTip = false;
+            this.toolStripbtnOpenAuth.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripbtnOpenAuth.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripbtnOpenAuth.Name = "toolStripbtnOpenAuth";
+            this.toolStripbtnOpenAuth.Size = new System.Drawing.Size(149, 25);
+            this.toolStripbtnOpenAuth.Text = "Set Google APi credentials";
+            this.toolStripbtnOpenAuth.Click += new System.EventHandler(this.toolStripbtnOpenAuth_Click);
+            // 
+            // toolStripProgressBar
+            // 
+            this.toolStripProgressBar.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            this.toolStripProgressBar.Name = "toolStripProgressBar";
+            this.toolStripProgressBar.Padding = new System.Windows.Forms.Padding(50, 0, 0, 0);
+            this.toolStripProgressBar.Size = new System.Drawing.Size(117, 25);
+            // 
+            // toolStripLbPathInfo
+            // 
+            this.toolStripLbPathInfo.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            this.toolStripLbPathInfo.Name = "toolStripLbPathInfo";
+            this.toolStripLbPathInfo.Size = new System.Drawing.Size(116, 28);
+            // 
+            // dgRename
+            // 
+            this.dgRename.AllowUserToAddRows = false;
+            this.dgRename.AllowUserToDeleteRows = false;
+            this.dgRename.BackgroundColor = System.Drawing.Color.White;
+            this.dgRename.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgRename.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.before,
+            this.after});
+            this.dgRename.Location = new System.Drawing.Point(14, 205);
+            this.dgRename.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.dgRename.Name = "dgRename";
+            this.dgRename.Size = new System.Drawing.Size(581, 375);
+            this.dgRename.TabIndex = 1;
+            this.dgRename.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgRename_CellClick);
+            this.dgRename.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgRename_CellValueChanged);
+            // 
+            // before
+            // 
+            this.before.HeaderText = "before";
+            this.before.Name = "before";
+            this.before.ReadOnly = true;
+            // 
+            // after
+            // 
+            this.after.HeaderText = "after";
+            this.after.Name = "after";
+            // 
+            // btnRunAPI
+            // 
+            this.btnRunAPI.Location = new System.Drawing.Point(678, 111);
+            this.btnRunAPI.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btnRunAPI.Name = "btnRunAPI";
+            this.btnRunAPI.Size = new System.Drawing.Size(88, 27);
+            this.btnRunAPI.TabIndex = 2;
+            this.btnRunAPI.Text = "Run API";
+            this.btnRunAPI.UseVisualStyleBackColor = true;
+            this.btnRunAPI.Click += new System.EventHandler(this.btnRunAPI_Click);
+            // 
+            // btnRefreash
+            // 
+            this.btnRefreash.Location = new System.Drawing.Point(678, 144);
+            this.btnRefreash.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btnRefreash.Name = "btnRefreash";
+            this.btnRefreash.Size = new System.Drawing.Size(88, 27);
+            this.btnRefreash.TabIndex = 3;
+            this.btnRefreash.Text = "Refreash";
+            this.btnRefreash.UseVisualStyleBackColor = true;
+            this.btnRefreash.Click += new System.EventHandler(this.btnRefreash_Click);
+            // 
+            // btnRenameAll
+            // 
+            this.btnRenameAll.Location = new System.Drawing.Point(785, 111);
+            this.btnRenameAll.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btnRenameAll.Name = "btnRenameAll";
+            this.btnRenameAll.Size = new System.Drawing.Size(88, 27);
+            this.btnRenameAll.TabIndex = 4;
+            this.btnRenameAll.Text = "Rename all";
+            this.btnRenameAll.UseVisualStyleBackColor = true;
+            this.btnRenameAll.Click += new System.EventHandler(this.btnRenameAll_Click);
+            // 
+            // lbStatus
+            // 
+            this.lbStatus.AutoSize = true;
+            this.lbStatus.Location = new System.Drawing.Point(923, 117);
+            this.lbStatus.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbStatus.Name = "lbStatus";
+            this.lbStatus.Size = new System.Drawing.Size(22, 15);
+            this.lbStatus.TabIndex = 5;
+            this.lbStatus.Text = ".....";
+            // 
+            // lbImage
+            // 
+            this.lbImage.Location = new System.Drawing.Point(653, 205);
+            this.lbImage.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.lbImage.Name = "lbImage";
+            this.lbImage.Size = new System.Drawing.Size(365, 375);
+            this.lbImage.TabIndex = 6;
+            this.lbImage.TabStop = false;
+            // 
+            // tbConsole
+            // 
+            this.tbConsole.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.tbConsole.Location = new System.Drawing.Point(14, 48);
+            this.tbConsole.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.tbConsole.Multiline = true;
+            this.tbConsole.Name = "tbConsole";
+            this.tbConsole.ReadOnly = true;
+            this.tbConsole.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tbConsole.Size = new System.Drawing.Size(453, 122);
+            this.tbConsole.TabIndex = 7;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1074, 623);
+            this.Controls.Add(this.tbConsole);
+            this.Controls.Add(this.lbImage);
+            this.Controls.Add(this.lbStatus);
+            this.Controls.Add(this.btnRenameAll);
+            this.Controls.Add(this.btnRefreash);
+            this.Controls.Add(this.btnRunAPI);
+            this.Controls.Add(this.dgRename);
+            this.Controls.Add(this.toolStrip1);
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgRename)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lbImage)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripbtnOpenFolder;
+        private System.Windows.Forms.ToolStripButton toolStripbtnOpenAuth;
+        private System.Windows.Forms.ToolStripProgressBar toolStripProgressBar;
+        private System.Windows.Forms.DataGridView dgRename;
+        private System.Windows.Forms.DataGridViewTextBoxColumn before;
+        private System.Windows.Forms.DataGridViewTextBoxColumn after;
+        private System.Windows.Forms.Button btnRunAPI;
+        private System.Windows.Forms.Button btnRefreash;
+        private System.Windows.Forms.Button btnRenameAll;
+        private System.Windows.Forms.Label lbStatus;
+        private System.Windows.Forms.PictureBox lbImage;
+        private System.Windows.Forms.TextBox tbConsole;
+        private System.Windows.Forms.ToolStripTextBox toolStripLbPathInfo;
+    }
+}
+
---
+++ Renamer/Form1.cs
@@ -0,0 +1,294 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using OpenSlideNET;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+using Vision = Google.Cloud.Vision.V1;
+using System.Text.RegularExpressions;
+
+namespace Renamer
+{
+    public partial class MainForm : Form
+    {
+
+        public string[] filesPath;
+        public List<string> arrRename;
+        public string google_api_credentials;
+        public string basePath;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Resize image
+        /// </summary>
+        /// <param name="imgToResize"></param>
+        /// <param name="size"></param>
+        /// <returns></returns>
+        public static Image resizeImage(Image imgToResize, Size size)
+        {
+            return (Image)(new Bitmap(imgToResize, size));
+        }
+
+        /// <summary>
+        /// Given a byte array in the format of bgra, convert it to <c>System.drawing.Image</c> /
+        /// </summary>
+        /// <param name="arr"></param>
+        /// <param name="width"></param>
+        /// <param name="height"></param>
+        /// <returns></returns>
+        public static Image ImageFromRawBgraArray(byte[] arr, int width, int height)
+        {
+            var output = new Bitmap(width, height);
+            var rect = new Rectangle(0, 0, width, height);
+            var bmpData = output.LockBits(rect,
+                ImageLockMode.ReadWrite, output.PixelFormat);
+            var ptr = bmpData.Scan0;
+            Marshal.Copy(arr, 0, ptr, arr.Length);
+            output.UnlockBits(bmpData);
+            return output;
+        }
+
+        public byte[] ImageToByteArray(System.Drawing.Image imageIn)
+        {
+            using (var ms = new MemoryStream())
+            {
+                imageIn.Save(ms, ImageFormat.Bmp);
+                return ms.ToArray();
+            }
+        }
+
+        public Image GetImage(string path)
+        {
+
+            Image img;
+            byte[] temp;
+            try
+            {
+                using (OpenSlideImage image = OpenSlideImage.Open(path))
+                {
+                    var haha = new OpenSlideImage.ImageDimensions();
+                    temp = image.ReadAssociatedImage("label", out haha);
+                    img = ImageFromRawBgraArray(temp, (int)haha.Width, (int)haha.Height);
+                    img.RotateFlip(RotateFlipType.Rotate90FlipNone);
+                }
+
+            }
+            catch
+            {
+                tbConsole.AppendText($"ERROR:: Failed reading slide\r\n");
+                lbStatus.Text = "Failed opening label image";
+                return null;
+
+            }
+            return img;
+        }
+
+        public String ProcessResult(String result)
+        {
+            result = Regex.Replace(result, "\n", " ");
+            string[] subs = result.Split(' ');
+            string haha = string.Join("-", subs);
+            return Regex.Replace(haha, @"[^0-9a-zA-Z\._-]", ""); ;
+        }
+
+        private void updateDataGrid(bool clean_rename_cache = true)
+        {
+            dgRename.Rows.Clear();
+            if (basePath is null) return;
+            filesPath = Directory.GetFiles(basePath);
+            if (clean_rename_cache)
+            {
+                arrRename = new List<String>();
+                for (int i = 0; i < filesPath.Length; i++) arrRename.Add("");
+            }
+            for (int i = 0; i < filesPath.Length; i++)
+            {
+                dgRename.Rows.Add(Path.GetFileName(filesPath[i]), arrRename[i]);
+                dgRename.Rows[i].HeaderCell.Value = String.Format("{0}", i + 1);
+            }
+        }
+
+        private void toolStripbtnOpenFolder_Click(object sender, EventArgs e)
+        {
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    filesPath = Directory.GetFiles(fbd.SelectedPath);
+                    arrRename = new List<String>();
+                    int num_files = filesPath.Length;
+
+                    basePath = fbd.SelectedPath;
+                    toolStripLbPathInfo.Text = "Current location: " + fbd.SelectedPath;
+                    tbConsole.AppendText(String.Format("Opened folder {0}", fbd.SelectedPath) + Environment.NewLine);
+                    tbConsole.AppendText(String.Format("{0} files found", num_files) + Environment.NewLine);
+
+                    for (int i = 0; i < filesPath.Length; i++)
+                    {
+                        arrRename.Add("");
+                    }
+
+                }
+
+            }
+
+            updateDataGrid();
+
+        }
+
+        private void toolStripbtnOpenAuth_Click(object sender, EventArgs e)
+        {
+            using (var fbd = new OpenFileDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.FileName))
+                {
+                    google_api_credentials = fbd.FileName;
+                    tbConsole.AppendText("Set Google api credentials location as " + google_api_credentials + "\r\n");
+                }
+
+            }
+        }
+
+        private void btnRenameAll_Click(object sender, EventArgs e)
+        {
+            toolStripProgressBar.Maximum = arrRename.Count;
+            toolStripProgressBar.Value = 0;
+            for (int i = 0; i < filesPath.Length; i++)
+            {
+                if (arrRename[i] == "") continue;
+                String temp = Path.Join(basePath, arrRename[i]);
+                String newName = temp;
+                newName = Path.GetExtension(newName) == "" ? newName + ".svs" : newName;
+
+                Func<int, int> find_digit = x => x.ToString().Length > 3 ? 0 : 3 - x.ToString().Length;
+
+                int counter = 2;
+                while (filesPath[i].ToUpper() != newName.ToUpper() && File.Exists(newName))
+                {
+                    String extension = Path.GetExtension(temp) == "" ? ".svs" : Path.GetExtension(temp);
+                    newName = Path.ChangeExtension(temp, null) + $"_{new String('0', find_digit(counter))}{counter}"
+                                + extension;
+                    counter++;
+                }
+                //tbConsole.AppendText($"from {filesPath[i]} to {newName}\r\n");
+                File.Move(filesPath[i], newName);
+                toolStripProgressBar.Value++;
+            }
+            lbStatus.Text = "Rename sucessfully\n";
+
+            updateDataGrid();
+        }
+
+        private void btnRefreash_Click(object sender, EventArgs e)
+        {
+            updateDataGrid();
+            tbConsole.AppendText("Refreshed\r\n");
+            lbStatus.Text = "Refreash sucessfully\n";
+        }
+
+        private async void btnRunAPI_Click(object sender, EventArgs e)
+        {
+            if (google_api_credentials is null)
+            {
+                MessageBox.Show("No Google api credentials is specificed");
+                return;
+            }
+
+            tbConsole.AppendText("Running Google vision API ...\r\n");
+            toolStripProgressBar.Maximum = filesPath.Length;
+            toolStripProgressBar.Value = 0;
+
+            Vision.ImageAnnotatorClient client = null;
+            try
+            {
+                client = new Vision.ImageAnnotatorClientBuilder
+                {
+                    CredentialsPath = google_api_credentials
+                }.Build();
+            }
+            catch
+            {
+                MessageBox.Show("Probably something wrong with your credentials, try to reset it");
+                tbConsole.AppendText("Task end due to error\r\n");
+                return;
+            }
+            //var image = Vision.Image.FromBytes(ImageToByteArray())
+
+            for (int i = 0; i < filesPath.Length; i++)
+            {
+                if (Path.GetExtension(filesPath[i]) != ".svs") continue;
+                Image temp = GetImage(filesPath[i]);
+                if (temp is null) continue;
+                byte[] temp_arr = ImageToByteArray(temp);
+                var image = Vision.Image.FromBytes(temp_arr);
+                var imageContext = new Vision.ImageContext();
+                imageContext.LanguageHints.Add("en");
+                var task = client.DetectDocumentTextAsync(image, imageContext);
+                var item = await task;
+                arrRename[i] = ProcessResult(item.Text);
+                dgRename.Rows[i].Cells[1].Value = arrRename[i];
+                toolStripProgressBar.Value++;
+                //updateDataGrid(false);
+            }
+            toolStripProgressBar.Value = toolStripProgressBar.Maximum;
+
+            tbConsole.AppendText("done\r\n");
+        }
+
+
+        private void dgRename_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            int row = e.RowIndex;
+            int col = e.ColumnIndex;
+            String v = "null";
+            String oldV = "null";
+
+            if (row == -1 || col == -1) return;
+            if (row >= 0 && row < dgRename.RowCount)
+            {
+                if (dgRename.Rows[row].Cells[col].Value is null) v = "";
+                else { v = dgRename.Rows[row].Cells[col].Value.ToString(); };
+                oldV = arrRename[row];
+                arrRename[row] = v;
+            }
+        }
+        ///
+        private void dgRename_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+            if (e.RowIndex < 0 || e.RowIndex > filesPath.Length - 1) return;
+            if (Path.GetExtension(filesPath[e.RowIndex]) != ".svs") return;
+
+            Image img = GetImage(filesPath[e.RowIndex]);
+            if (img is null) return;
+
+            int w = lbImage.Width;
+            int h = lbImage.Height;
+
+            int i = img.Width;
+            int j = img.Height;
+
+            int new_w = (int)((float)i / (float)Math.Max(i, j) * (float)w);
+            int new_h = (int)((float)j / (float)Math.Max(i, j) * (float)h);
+
+            img = resizeImage(img, new Size(new_w, new_h));
+            lbImage.Image = img;
+
+            lbStatus.Text = "Opened label image";
+        }
+
+    }
+}
